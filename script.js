c93435ac2284eb255293dfa90a8036ebaaf6cf2c
Moyudin Papu <moinpapu92446@gmail.com>
2023-12-03 06:20:05 +0530

    I Have almost Completed with portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+var professions = ["Web Developer.", "Programmer.", "Web Designer."];
+var currentProfessionIndex = 0;
+
+function changeProfession() {
+    var professionElement = document.getElementById("profession");
+    currentProfessionIndex = (currentProfessionIndex + 1) % professions.length;
+    professionElement.textContent = professions[currentProfessionIndex];
+}
+
+setInterval(changeProfession, 3000);
